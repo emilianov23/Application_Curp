2086dee91f87a5845091902679ea398903637d24
emilianov23 <emilianov23@outlook.com>
2018-09-12 01:36:59 -0500

    Initial commit

---
+++ app/src/main/java/com/example/emiliano/application_curp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.emiliano.application_curp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Curp cur = getIntent().getParcelableExtra("cur");
+
+        ((TextView)findViewById(R.id.nombre)).setText(cur.nombre());
+        ((TextView)findViewById(R.id.apellido1)).setText(cur.apellido1());
+        ((TextView)findViewById(R.id.apellido2)).setText(cur.apellido2());
+        ((TextView)findViewById(R.id.sex)).setText(cur.sex());
+        ((TextView)findViewById(R.id.fecha)).setText(cur.fecha());
+        ((TextView)findViewById(R.id.entidad)).setText(cur.entidad());
+    }
+    @Override
+    public boolean onCreateOptionsMenu(menu menu) {
+        menuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+
+}
